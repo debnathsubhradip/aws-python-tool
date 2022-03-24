d886910ee613fc7bdd1dc90f6d381c20122a434a
debnath.subhradip <debnath.subhradip@gmail.com>
2022-03-24 16:21:55 +0530

    old variable file deleted. gitignore file added

--- main.py
+++ main.py
@@ -3,8 +3,34 @@ import os
 from dotenv import load_dotenv
 load_dotenv('variable.env')
 check=os.getenv("TEST")
+flag=1
+def options(argument):
+    
+    command={
+        "exit":"Exit out of the program",
+        "1":"Check account details"
+    }
+    if argument=="all":
+        return command
+    else:
+        return command.get(argument)
+
+def menu():
+    while(flag!=0):
+        menus=options('all')
+        
+        print('''
+        |||\t\t\tAWS PYTHON TOOL\t\t\t||| 
+        ''')
+        # print(menus['1'])
+        for item in menus.keys():
+            print("\t"+item+"\t:\t"+menus[item])
+        command=input("\n\t|||\t"+"Enter Command"+"\t"+":")
+        print(options(command))
+
+
 def nameCheck(name):
     # print ("This is {name}".format(name=name))
     print (f"This is {name}")
 if __name__=="__main__":
-    nameCheck(check)
+    menu()
