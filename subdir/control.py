af713bb2585a532d50e0923bd4fa8be3b98c9488
debnath.subhradip <debnath.subhradip@gmail.com>
2022-03-25 01:01:23 +0530

    minute changes made, debugged

--- subdir/control.py
+++ subdir/control.py
@@ -1,6 +1,7 @@
 import boto3
 import os
 from dotenv import load_dotenv
+import json
 load_dotenv()
 accesskeyid=os.getenv("ACCESS-KEY-ID")
 secretaccesskey=os.getenv("SECRET-ACCESS-KEY")
@@ -15,4 +16,20 @@ def check_service():
     print('Available Services :')
     for item in available_services:
         print(str(item))
+
+
+# check the code below this --> This was to check if the Instances are running fine or not. and by how much and its details
+def check_ec2_service():
+    client = session.client("ec2", region_name="us-west-2")
+    response = client.describe_instances(
+    Filters=[
+        {   'Name': 'instance-state-name',
+            'Values': ['running']
+        }
+    ]
+)
+    return response
+
+print(json.dumps(check_ec2_service()))
+# check_ec2_service()
 
