c03ef07492328f4e2f60d743c3c48d4a749230ad
debnath.subhradip <debnath.subhradip@gmail.com>
2022-03-24 16:53:50 +0530

    small changes made to control.py and gitignore

--- subdir/control.py
+++ subdir/control.py
@@ -1,7 +1,7 @@
 import boto3
 import os
 from dotenv import load_dotenv
-load_dotenv('keys.env')
+load_dotenv()
 accesskeyid=os.getenv("ACCESS-KEY-ID")
 secretaccesskey=os.getenv("SECRET-ACCESS-KEY")
 
